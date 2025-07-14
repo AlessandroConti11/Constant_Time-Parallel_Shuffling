4adb4a72fed15c89d264449f7722073585165932
alessandro-conti <alessandro11.conti@mail.polimi.it>
2025-07-14 15:57:52 +0200

    Constant_Time-Parallel_Shuffling - utility - master: implemented safeRealloc

---
+++ utility/safeRealloc.c
@@ -0,0 +1,23 @@
+#include "safeRealloc.h"
+
+
+/**
+ * New realloc version.
+ *
+ * @note If reallocSize is 0, this function allocates 1 byte instead of zero, to avoid problems with realloc implementations that may return NULL.
+ *
+ * @param pointerToRealloc the pointer to memory block to be reallocate.
+ * @param reallocSize the new memory block size.
+ * @return the pointer to the new memory block.
+ */
+void *safeRealloc(void *pointerToRealloc, size_t reallocSize) {
+    /// New pointer.
+    void *newPointer = realloc(pointerToRealloc, reallocSize ? reallocSize : 1);
+
+
+    if (!newPointer) {
+        perror("realloc"); exit(EXIT_FAILURE);
+    }
+
+    return newPointer;
+}
--- utility/safeRealloc.h
+++ utility/safeRealloc.h
@@ -9,26 +9,7 @@
 #include <time.h>
 
 
-/**
- * New realloc version.
- *
- * @note If reallocSize is 0, this function allocates 1 byte instead of zero, to avoid problems with realloc implementations that may return NULL.
- *
- * @param pointerToRealloc the pointer to memory block to be reallocate.
- * @param reallocSize the new memory block size.
- * @return the pointer to the new memory block.
- */
-static void *safeRealloc(void *pointerToRealloc, size_t reallocSize) {
-    /// New pointer.
-    void *newPointer = realloc(pointerToRealloc, reallocSize ? reallocSize : 1);
-
-
-    if (!newPointer) {
-        perror("realloc"); exit(EXIT_FAILURE);
-    }
-
-    return newPointer;
-}
+void *safeRealloc(void *pointerToRealloc, size_t reallocSize);
 
 
 #endif //DJB_SAFEREALLOC_H
