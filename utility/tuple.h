2e59342b3e291beaeaa1290bdb9c8a16518e5d0a
alessandro-conti <alessandro11.conti@mail.polimi.it>
2025-07-14 10:19:52 +0200

    Constant_Time-Parallel_Shuffling - utility - master: implemented tuple

---
+++ utility/tuple.h
@@ -0,0 +1,18 @@
+#ifndef DJB_TUPLE_H
+#define DJB_TUPLE_H
+
+
+typedef struct {
+    int index0;
+    int index1;
+} Pair;
+
+typedef struct {
+    int index0;
+    int fromLeft;
+    int indexInItsList;
+    int index1;
+} Quadruple;
+
+
+#endif //DJB_TUPLE_H
